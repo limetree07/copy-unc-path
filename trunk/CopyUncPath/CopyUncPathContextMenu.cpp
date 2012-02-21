9b95fd1d66aa778a7d84ce909e7c8da55074cdba
limetree07@gmail.com <limetree07@gmail.com@110bfd3a-edde-a4ff-4b94-686aef6fc3ff>
2012-02-21 22:50:17 +0000

    Added new project with C++ port of shell context menu extension.

---
+++ trunk/CopyUncPath/CopyUncPathContextMenu.cpp
@@ -0,0 +1,243 @@
+/*
+ * Copyright 2012 limetree07. All rights reserved.
+ *	
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "stdafx.h"
+#include "CopyUncPathContextMenu.h"
+#include <strsafe.h>
+#include <Winnetwk.h>
+#include <tchar.h>
+
+#pragma comment(lib, "mpr.lib")
+
+extern HINSTANCE g_hInst;
+
+#define IDM_DISPLAY			0			// The command's identifier offset. 
+#define VERB_SAMPLEA		"CopyUncPath"	// The command's ANSI verb string
+#define VERB_SAMPLEW		L"CopyUncPath"	// The command's Unicode verb string
+
+void CCopyUncPathContextMenu::OnCopyFileName(HWND hWnd)
+{
+	DWORD dwRetVal;
+
+	WCHAR Buffer[1024];
+	DWORD dwBufferLength = 1024;
+
+	UNIVERSAL_NAME_INFO * unameinfo;
+
+	unameinfo = (UNIVERSAL_NAME_INFO *) &Buffer;
+	dwRetVal = ::WNetGetUniversalName(m_szSelectedFile, UNIVERSAL_NAME_INFO_LEVEL, (LPVOID) unameinfo, &dwBufferLength );
+
+	if (dwRetVal == NO_ERROR)
+	{
+		SetClipboardString(unameinfo->lpUniversalName, hWnd);
+	}
+	else if (dwRetVal == ERROR_NOT_CONNECTED)
+	{
+		SetClipboardString(m_szSelectedFile, hWnd);
+	}
+}
+
+void CCopyUncPathContextMenu::SetClipboardString(LPWSTR uncPath, HWND hWnd)
+{
+	DWORD len = wcslen(uncPath);
+	HGLOBAL hdst;
+	LPWSTR dst;
+
+	hdst = GlobalAlloc(GMEM_MOVEABLE | GMEM_DDESHARE, (len + 1) * sizeof(WCHAR));
+	dst = (LPWSTR)GlobalLock(hdst);
+	memcpy(dst, uncPath, len * sizeof(WCHAR));
+	dst[len] = 0;
+	GlobalUnlock(hdst);
+
+	if (!OpenClipboard(hWnd))
+	{
+		return;
+	}
+	EmptyClipboard(); 
+
+	if (this->m_szSelectedFile == NULL)
+	{
+		CloseClipboard();
+		return;
+	}
+
+	SetClipboardData(CF_UNICODETEXT, hdst);
+	CloseClipboard();
+}
+
+#pragma region IShellExtInit
+
+IFACEMETHODIMP CCopyUncPathContextMenu::Initialize(LPCITEMIDLIST pidlFolder, LPDATAOBJECT pDataObj, HKEY hProgID)
+{
+	if (NULL == pDataObj)
+	{
+		return E_INVALIDARG;
+	}
+
+	HRESULT hr = E_FAIL;
+
+	FORMATETC fe = { CF_HDROP, NULL, DVASPECT_CONTENT, -1, TYMED_HGLOBAL };
+	STGMEDIUM stm;
+
+	// The pDataObj pointer contains the objects being acted upon. In this 
+	// example, we get an HDROP handle for enumerating the selected files and 
+	// folders.
+	if (SUCCEEDED(pDataObj->GetData(&fe, &stm)))
+	{
+		// Get an HDROP handle.
+		HDROP hDrop = static_cast<HDROP>(GlobalLock(stm.hGlobal));
+		if (hDrop != NULL)
+		{
+			// Determine how many files are involved in this operation. This 
+			// code sample displays the custom context menu item when only 
+			// one file is selected. 
+			UINT nFiles = DragQueryFile(hDrop, 0xFFFFFFFF, NULL, 0);
+			if (nFiles == 1)
+			{
+				// Get the path of the file.
+				if (0 != DragQueryFile(hDrop, 0, m_szSelectedFile, 
+					ARRAYSIZE(m_szSelectedFile)))
+				{
+					hr = S_OK;
+				}
+			}
+
+			GlobalUnlock(stm.hGlobal);
+		}
+
+		ReleaseStgMedium(&stm);
+	}
+
+	// If any value other than S_OK is returned from the method, the context 
+	// menu item is not displayed.
+	return hr;
+}
+
+#pragma endregion
+
+#pragma region IContextMenu
+
+IFACEMETHODIMP CCopyUncPathContextMenu::QueryContextMenu(
+	HMENU hMenu, UINT indexMenu, UINT idCmdFirst, UINT idCmdLast, UINT uFlags)
+{
+	if (CMF_DEFAULTONLY & uFlags)
+	{
+		return MAKE_HRESULT(SEVERITY_SUCCESS, 0, USHORT(0));
+	}
+
+	HICON hIcon = (HICON)LoadImage(g_hInst, MAKEINTRESOURCE(IDI_COPYUNC), IMAGE_ICON, 16, 16, LR_DEFAULTCOLOR);
+
+	MENUITEMINFO mii = { sizeof(mii) };
+	mii.fMask = MIIM_BITMAP | MIIM_STRING | MIIM_FTYPE | MIIM_ID | MIIM_STATE;
+	mii.wID = idCmdFirst + IDM_DISPLAY;
+	mii.fType = MFT_STRING;
+	mii.dwTypeData = _T("&Copy UNC Path");
+	mii.hbmpItem = m_bitmapConverter.IconTo32BitBitmap(hIcon);
+
+	if (!InsertMenuItem(hMenu, indexMenu, TRUE, &mii))
+	{
+		return HRESULT_FROM_WIN32(GetLastError());
+	}
+
+	DestroyIcon(hIcon);
+
+	//InsertMenu(hMenu, indexMenu + 1, MFT_SEPARATOR | MF_BYPOSITION, idCmdFirst + 
+	//	IDM_DISPLAY, _T(""));
+
+	return MAKE_HRESULT(SEVERITY_SUCCESS, 0, USHORT(IDM_DISPLAY + 1));
+}
+
+IFACEMETHODIMP CCopyUncPathContextMenu::GetCommandString(
+	UINT_PTR idCommand, UINT uFlags, LPUINT lpReserved, LPSTR pszName, 
+	UINT uMaxNameLen)
+{
+	HRESULT hr = E_INVALIDARG;
+
+	if (idCommand == IDM_DISPLAY)
+	{
+		switch (uFlags)
+		{
+		case GCS_VERBA:
+			hr = StringCchCopyNA(pszName, 
+				lstrlenA(pszName) / sizeof(pszName[0]), 
+				VERB_SAMPLEA, uMaxNameLen);
+			break;
+
+		case GCS_VERBW:
+			hr = StringCchCopyNW((LPWSTR)pszName, 
+				lstrlenW((LPWSTR)pszName) / sizeof(pszName[0]), 
+				VERB_SAMPLEW, uMaxNameLen);
+			break;
+
+		default:
+			hr = S_OK;
+		}
+	}
+
+	return hr;
+}
+
+IFACEMETHODIMP CCopyUncPathContextMenu::InvokeCommand(LPCMINVOKECOMMANDINFO pici)
+{
+	BOOL fUnicode = FALSE;
+
+	if (pici->cbSize == sizeof(CMINVOKECOMMANDINFOEX))
+	{
+		if((pici->fMask & CMIC_MASK_UNICODE))
+		{
+			fUnicode = TRUE;
+		}
+	}
+
+	if (!fUnicode && HIWORD(pici->lpVerb))
+	{
+		if (StrCmpIA(pici->lpVerb, VERB_SAMPLEA) == 0)
+		{
+			OnCopyFileName(pici->hwnd);
+		}
+		else
+		{
+			return E_FAIL;
+		}
+	}
+	else if(fUnicode && HIWORD(((CMINVOKECOMMANDINFOEX*)pici)->lpVerbW))
+	{
+		if (StrCmpIW(((CMINVOKECOMMANDINFOEX*)pici)->lpVerbW, 
+			VERB_SAMPLEW) == 0)
+		{
+			OnCopyFileName(pici->hwnd);
+		}
+		else
+		{
+			return E_FAIL;
+		}
+	}
+	else
+	{
+		if (LOWORD(pici->lpVerb) == IDM_DISPLAY)
+		{
+			OnCopyFileName(pici->hwnd);
+		}
+		else
+		{
+			return E_FAIL;
+		}
+	}
+
+	return S_OK;
+}
+
+#pragma endregion
