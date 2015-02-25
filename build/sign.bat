1b42334ae8227405a8717e9e6b7933cc7f673849
limetree07@gmail.com <limetree07@gmail.com@110bfd3a-edde-a4ff-4b94-686aef6fc3ff>
2015-02-25 19:57:07 +0000

    creating v1 branch for .net further enhancements

--- build/sign.bat
+++
@@ -1,2 +0,0 @@
-set SDKDIR="C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\bin"
-%SDKDIR%\signtool.exe sign /f ..\src\limetree07.pfx /p %1 /t "http://www.trustcenter.de/codesigning/timestamp" %2
