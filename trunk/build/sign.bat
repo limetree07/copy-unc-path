3b758afa305a7f640f319648485ee51a6b445932
limetree07@gmail.com <limetree07@gmail.com@110bfd3a-edde-a4ff-4b94-686aef6fc3ff>
2015-02-26 11:48:31 +0000

    changed signing to new certificate

--- trunk/build/sign.bat
+++ trunk/build/sign.bat
@@ -1,2 +1,2 @@
 set SDKDIR="C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\bin"
-%SDKDIR%\signtool.exe sign /f ..\src\limetree07.pfx /p %1 /t "http://timestamp.verisign.com/scripts/timstamp.dll" %2
+%SDKDIR%\signtool.exe sign /f ..\src\copyuncpath.pfx /t "http://timestamp.verisign.com/scripts/timstamp.dll" %1
